56f74ed8052738a5a61d64692af669c247cbeb74
dongmin kim <emthrology@gmail.com>
2021-12-16 17:34:08 +0900

    controlled component - v-model 의 구조적 특성 활용한 테크닉

--- controlled/src/App.vue
+++ controlled/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <check-box v-model:value="checked"></check-box>
+  <check-box v-model="checked"></check-box>
 </template>
 
 <script>
@@ -15,4 +15,7 @@ export default {
     }
   }
 }
+//v-model이 돌아가는 구조를 통해 direct props mutations 방지
+//동시에 상하위 컴포넌트 결합도 떡상
+//datepicker, callendar, modal 등 세부적인 컴포넌트 작성시 유용한 테크닉
 </script>
