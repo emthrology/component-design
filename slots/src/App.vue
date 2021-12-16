4a6f3619f8488a5872a6c1fd4baf3d5c087ed038
dongmin kim <emthrology@gmail.com>
2021-12-16 17:17:39 +0900

    slot usage - default slot, custom slot

--- slots/src/App.vue
+++ slots/src/App.vue
@@ -7,12 +7,16 @@
       <item>아이템 4</item>
       <item>아이템 5</item> -->
       <item v-for="item of items" :key="item.id">
-        <template v-slot:title>
-          {{ item.title }}
+        <template v-if="item.title" v-slot:title>
+          <h3>{{ item.title }}</h3>
         </template>
         <template v-if="item.content" v-slot:content>
+          <hr />
           {{ item.content }}
         </template>
+      </item>
+      <item>
+        디폴트 아이템 <button style="color:blue"> testButton </button>
       </item>
     </ul>
   </div>
@@ -30,7 +34,8 @@ export default {
       items: [
         { id: 1, title: "아이템1", content: "콘텐트1" },
         { id: 2, title: "아이템2", content: "콘텐트2" },
-        { id: 3, title: "아이템3" }
+        { id: 3, title: "아이템3" },
+        { id: 3, content: "콘텐트4" }
       ],
     };
   },
--- slots/src/Item.vue
+++ slots/src/Item.vue
@@ -1,11 +1,9 @@
 <template>
   <li>
-    <h3>
-      <slot name="title">
-        <!-- NOTE: 등록하는 곳에서 정의할 화면 영역 -->
-      </slot>
-    </h3>
-    <hr />
+    <slot name="title">
+      <!-- NOTE: 등록하는 곳에서 정의할 화면 영역 -->
+    </slot>
     <slot name="content"></slot>
+    <slot></slot>
   </li>
 </template>
