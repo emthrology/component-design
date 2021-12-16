5744bf91bf1ac2a5ec3369cf2b5961754a83afc9
dongmin kim <emthrology@gmail.com>
2021-12-16 16:59:20 +0900

    slot usage - named slot, with v-for

--- slots/src/App.vue
+++ slots/src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
     <ul>
-      <item>아이템 1</item>
+      <!-- <item>아이템 1</item>
       <item>아이템 2</item>
       <item>아이템 3</item>
       <item>아이템 4</item>
-      <item>아이템 5</item>
+      <item>아이템 5</item> -->
+      <item v-for="item of items" :key="item.id">
+        <template v-slot:title>
+          {{ item.title }}
+        </template>
+        <template v-slot:content>
+          {{ item.content }}
+        </template>
+      </item>
     </ul>
   </div>
 </template>
 
 <script>
-import Item from './Item.vue';
+import Item from "./Item.vue";
 
 export default {
   components: {
     Item,
   },
-}
+  data() {
+    return {
+      items: [
+        { id: 1, title: "아이템1", content: "콘텐트1" },
+        { id: 2, title: "아이템2", content: "콘텐트2" },
+      ],
+    };
+  },
+};
 </script>
--- slots/src/Item.vue
+++ slots/src/Item.vue
@@ -1,7 +1,11 @@
 <template>
   <li>
-    <slot>
-      <!-- NOTE: 등록하는 곳에서 정의할 화면 영역 -->
-    </slot>
+    <h3>
+      <slot name="title">
+        <!-- NOTE: 등록하는 곳에서 정의할 화면 영역 -->
+      </slot>
+    </h3>
+    <hr />
+    <slot name="content"></slot>
   </li>
 </template>
