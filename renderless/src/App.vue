756b4162896ddbe7a070495f67da3956bde04672
dongmin kim <emthrology@gmail.com>
2021-12-17 10:58:08 +0900

    renderless : 컴포넌트에 로직이 들어가는 역설계

--- renderless/src/App.vue
+++ renderless/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+    <fetch-data
+      v-slot="{ response, loading }"
+      url="https://jsonplaceholder.typicode.com/users/1"
+    >
       <!-- ... response, loading 을 가져올수 있다-->
-      <div v-slot="{response, loading}">
+      <div>
         <div v-if="!loading">
-          <p>{{response.name}}</p>
-          <p>{{response.email}}</p>
+          <p v-for="el of Object.entries(response)">
+            {{el[0]}} : {{el[1]}}
+          </p>
+          <!-- <p>{{ response.name }}</p>
+          <p>{{ response.email }}</p> -->
         </div>
         <div v-if="loading">
           loading...
@@ -16,11 +22,11 @@
 </template>
 
 <script>
-import FetchData from './components/FetchData.vue'
+import FetchData from "./components/FetchData.vue";
 
 export default {
   components: {
-    FetchData
+    FetchData,
   },
-}
+};
 </script>
